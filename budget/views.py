e03fef58833e0c25a66671b72ae3b29f2fa55275
anandhu <anandhu13vijayan@gmail.com>
2024-02-15 15:33:06 +0530

    initial commit

--- budget/views.py
+++ budget/views.py
@@ -1,3 +1,313 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+from django.views.generic import View
+
+from budget.models import Transaction
+
+from django import forms
+
+from django.contrib.auth.models import User
+
+from django.contrib.auth import authenticate,login,logout
+
+from django.utils import timezone
+
+from django.db.models import Sum
+
+from django.utils.decorators import method_decorator
+
+from django.contrib import messages
+
+from django.views.decorators.cache import never_cache
+
+
+
+
+
+
+def signin_required(fn):
+
+    def wrapper(request,*args,**kwargs):
+        if not request.user.is_authenticated:
+
+            messages.error(request,"invalid session")
+
+            return redirect("signin")
+        else:
+            return fn(request,*args,**kwargs)
+    return wrapper
+
+
+
+decs=[signin_required,never_cache]
+
+
+
+
+class TransactionForm(forms.ModelForm):
+
+    class Meta:
+        model=Transaction
+        exclude=("created_date","user_object")
+        # fields="__all__"
+        # fields=["field1","field2",]
+        widgets={                                                             #for styling
+            "title":forms.TextInput(attrs={"class":"form-control"}),
+            "amount":forms.NumberInput(attrs={"class":"form-control"}),
+            "type":forms.Select(attrs={"class":"form-control form-select"}),
+            "category":forms.Select(attrs={"class":"form-control form-select"})
+
+        }
+
+
+
+class RegistrationForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=["username","email","password"]
+
+        widgets={
+            "username":forms.TextInput(attrs={"class":"form-control"}),
+            "email":forms.EmailInput(attrs={"class":"form-control"}),
+            "password":forms.PasswordInput(attrs={"class":"form-control"}),
+
+        }
+
+
+
+
+
+class LoginForm(forms.Form):
+
+    username=forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+    password=forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
+
+
+
+
+
+
+
+
+
+
+
+
+# create your views here.
+# view for listing all Transaction
+# url: localhost:8000/transactions/all/
+# method: get
+
+
+
+@method_decorator(decs,name="dispatch")
+
+class TransactionListview(View):
+    def get(self,request,*args,**kwargs):
+        
+        qs=Transaction.objects.filter(user_object=request.user)
+
+        current_month=timezone.now().month
+        current_year=timezone.now().year
+
+        print(current_month,current_year)
+        data=Transaction.objects.filter(
+             created_date__month=current_month,
+             created_date__year=current_year,
+             user_object=request.user
+        ).values("type").annotate(type_sum=Sum("amount"))
+        print(data)
+
+        cat_qs=Transaction.objects.filter(
+             created_date__month=current_month,
+             created_date__year=current_year,
+             user_object=request.user
+        ).values("category").annotate(cat_sum=Sum("amount"))
+        print(cat_qs)
+
+        # expense_total=Transaction.objects.filter(
+        #     user_object=request.user,
+        #     type="expense",
+        #     created_date__month=current_month,
+        #     created_date__year=current_year,
+        # ).aggregate(Sum("amount"))
+        # print(expense_total)
+
+        # income_total=Transaction.objects.filter(
+        #     user_object=request.user,
+        #     type="income",
+        #     created_date__month=current_month,
+        #     created_date__year=current_year
+        # ).aggregate(Sum("amount"))
+        # print(income_total)
+
+
+        return render(request,"transaction_list.html",{"data":qs,"type_total":data,"cat_total":cat_qs})
+    
+
+
+
+    
+# view for creating new transaction
+# url: localhost:8000/transactions/add/
+# method: get,post
+
+
+
+@method_decorator(decs,name="dispatch")
+
+class TransactioncreateView(View):
+    def get (self,request,*args,**kwargs):
+        form=TransactionForm()
+        return render (request,"transaction_add.html",{"form":form})
+
+
+    def post(self,request,*args,**kwargs):
+        form=TransactionForm(request.POST)
+        if form.is_valid():
+            
+            # form.save()
+            data=form.cleaned_data
+            Transaction.objects.create(**data,user_object=request.user)
+
+            messages.success(request,"Transaction has been added successfully")
+            return redirect("transaction-list")
+        else:
+            messages.error(request,"Failed to add transaction")
+            return render(request,"transaction_add.html",{"form":form})
+
+
+
+
+
+# Transaction detail view
+# url: localhost:8000/transactions/details/
+# method: get
+        
+
+@method_decorator(decs,name="dispatch")
+
+class TransactionDetailView(View):
+    def get(self,request,*args,**kwargs): 
+        id=kwargs.get("pk")
+        qs=Transaction.objects.get(id=id)
+        return render (request,"transaction_detail.html",{"data":qs})
+    
+
+
+# transaction delete
+# url: localhost:8000/transactions/{id}}/remove/
+# method get
+    
+@method_decorator(decs,name="dispatch")
+
+class TransactionDeleteView(View):
+    def get(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        Transaction.objects.filter(id=id).delete()
+        messages.success(request,"Transaction has been removed")
+        return redirect("transaction-list")
+    
+
+
+# transaction update
+# url: localhost:8000/transactions/{id}}/edit/
+# method get,post
+    
+@method_decorator(decs,name="dispatch")
+
+class TransactionUpdateView(View):
+    def get(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        transaction_object=Transaction.objects.get(id=id)
+        form=TransactionForm(instance=transaction_object)
+        return render(request,"transaction_edit.html",{"form":form})
+    
+    def post(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        transaction_object=Transaction.objects.get(id=id)
+
+        form=TransactionForm(request.POST,instance=transaction_object)
+        if form.is_valid():
+            form.save()
+            
+            messages.success(request,"Transaction has been updated successfully")
+            return redirect("transaction-list")
+        else:
+            messages.error(request,"Failed to update transaction")
+            return render(request,"transaction-edit.html",{"form":form})
+
+
+    
+
+
+# signup
+# url:localhost:8000/signup/
+# method:get,post
+        
+
+class SignUpView(View):
+    def get(self,request,*args,**kwargs):
+        form=RegistrationForm()
+        return render(request,"register.html",{"form":form})
+    
+    def post(self,request,*args,**kwargs):
+        form=RegistrationForm(request.POST)
+        if form.is_valid():
+            # form.save()            # password encrypt
+            User.objects.create_user(**form.cleaned_data)       # quary for password encrypting
+            print("Account created")
+            return redirect ("signin")
+        else:
+            print("failed")
+            return render(request,"register.html",{"form":form})
+
+
+    
+
+
+# signin
+# url:localhost:8000/signin/
+# method:get,post
+
+        
+class SignInView(View):
+    def get(self,request,*args,**kwargs):
+        form=LoginForm()
+        return render(request,"signin.html",{"form":form})
+    
+    def post(self,request,*args,**kwargs):
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            u_name=form.cleaned_data.get("username")
+            pwd=form.cleaned_data.get("password")
+            user_object=authenticate(request,username=u_name,password=pwd)
+            if user_object:
+                login(request,user_object)
+                
+                messages.success(request,"Signin completed successfully")
+                return redirect("transaction-list")
+
+        return render(request,"signin.html",{"form":form})
+    
+
+
+
+
+
+@method_decorator(decs,name="dispatch")
+
+class SignOutView(View):
+    def get(self,request,*args,**kwargs):
+        logout(request)
+        return redirect("signin")
+
+            
+
+
+
+
+        
+
+
+
--- fundflowapplication/urls.py
+++ fundflowapplication/urls.py
@@ -16,7 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from budget import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("transactions/all/",views.TransactionListview.as_view(),name="transaction-list"),
+    path("transactions/add/",views.TransactioncreateView.as_view(),name="transaction-add"),
+    path("transactions/<int:pk>/",views.TransactionDetailView.as_view(),name="transaction-detail"),
+    path("transactions/<int:pk>/remove/",views.TransactionDeleteView.as_view(),name="transaction-delete"),
+    path("transactions/<int:pk>/edit/",views.TransactionUpdateView.as_view(),name="transaction-update"),
+    path("signup/",views.SignUpView.as_view(),name="signup"),
+    path("",views.SignInView.as_view(),name="signin"),
+    path("signout/",views.SignOutView.as_view(),name="signout"),
+    
+
+
+
+    
 ]
